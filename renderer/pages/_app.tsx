5afb554acfc819ca9c5fc04a0739f23587dcf89d
EarthlyEric <minecarft.creeper.eric@gmail.com>
2023-04-01 13:51:20 +0800

    🖥️ wip : remove next page && change page name.

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -3,8 +3,8 @@ import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function lvs_launcher({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default lvs_launcher
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (with-typescript-tailwindcss)</title>
+        <title>LVS Launcher</title>
       </Head>
       <div className='grid grid-col-1 text-2xl w-full text-center'>
         <img className='ml-auto mr-auto' src='/images/logo.png' />
@@ -17,11 +17,6 @@ function Home() {
         <span>tailwindcss</span>
         <span>=</span>
         <span>💕 </span>
-      </div>
-      <div className='mt-1 w-full flex-wrap flex justify-center'>
-        <Link href='/next'>
-          <a className='btn-blue'>Go to next page</a>
-        </Link>
       </div>
     </React.Fragment>
   );
